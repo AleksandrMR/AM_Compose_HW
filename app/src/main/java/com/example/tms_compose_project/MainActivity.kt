4e3c50b3123f129311ca58b06b708e28c34ede5d
Sergey Katsapov <katsapov.sergey@mail.ru>
2023-04-26 23:39:58 +0300

    [ADD] - compose simple login screen ui;

--- app/src/main/java/com/example/tms_compose_project/MainActivity.kt
+++ app/src/main/java/com/example/tms_compose_project/MainActivity.kt
@@ -2,71 +2,90 @@ package com.example.tms_compose_project
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.SemanticsProperties.EditableText
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colors.background
-            ) {
-                LoginScreen(context = applicationContext)
-            }
+            LoginScreen()
         }
     }
 }
 
+@Preview
 @Composable
-fun LoginScreen(context: Context) {
+fun LoginScreen() {
 
-    var email by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Gray)
+            .padding(all = 20.dp)
+    ) {
+        var username by rememberSaveable { mutableStateOf("") }
+        var password by rememberSaveable { mutableStateOf("") }
 
-    Column() {
         Text(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 20.dp),
             text = "Login",
-            fontFamily = FontFamily.Monospace,
-            textAlign = TextAlign.Left,
-            fontSize = 40.sp,
+            fontFamily = FontFamily.Serif,
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
             fontWeight = FontWeight.Bold,
             color = Color.Red
         )
 
-//        OutlinedTextField(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(bottom = 10.dp, top = 10.dp),
-//            value = email,
-//            onValueChange = { email = "123123" },
-//            label = { Text(text = "Enter your email") },
-//            visualTransformation = PasswordVisualTransformation(),
-//            keyboardActions = KeyboardOptions(keyboardType = KeyboardType.Password)
-//        )
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp),
+            value = username,
+            onValueChange = {
+                username = it
+            },
+            label = { Text("Yor username") }
+        )
 
-        OutlinedButton(onClick = { /*TODO*/ }) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = { Text("Yor password") }
+        )
 
+        OutlinedButton(onClick = {
+
+        }) {
             Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp),
                 text = "Login",
                 textAlign = TextAlign.Center
             )
